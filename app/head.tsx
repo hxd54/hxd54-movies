81fc1f122d8171e53c931f69665e40294082634d
Haxdev / <166153160+hxd54@users.noreply.github.com>
2025-05-31 02:36:30 -0700

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="pqWIIKUJxbJ_p7KYkbE4rMycxhFrAsTMnA2GmI7xyuI" />
+    </>
+  );
+}
